ceb85d81d76f5cfeab84d6cc9d801b81a4e492b1
MihirYJoshi <43663025+MihirYJoshi@users.noreply.github.com>
2022-03-24 22:12:43 -0700

    a

--- matchStrategy.php
+++ matchStrategy.php
@@ -612,10 +612,6 @@ include("navBar.php"); ?>
 				<div class="table-responsive">
 					<table class="table">
 						<tbody>
-							<tr class="success">
-								<td>ELO</td>
-								<td><?php echo (getElo($teamNumber)); ?></td>
-							</tr>
 							<tr class="danger">
 								<td>DNP</td>
 								<td><?php echo (getTotalDNP($teamNumber)); ?></td>
