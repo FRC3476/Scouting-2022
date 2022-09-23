e8d74f980d02ae0b48e7c9d054005191d5bd0ce1
MihirYJoshi <43663025+MihirYJoshi@users.noreply.github.com>
2022-09-23 13:05:53 -0700

    Update teamRanking.php

--- teamRanking.php
+++ teamRanking.php
@@ -29,16 +29,16 @@ include("header.php") ?>
                     <th>Lead Gen ELO</th>
                     <th>Scouter Gen ELO</th>
                     <th>Avg Upper Shot Percentage</th>
+                    <th>Avg Teleop Upper Goal</th>
+                    <th>Avg Teleop Lower Goal</th>
+                    <th>Avg Auto Upper Goal</th>
+                    <th>Avg Auto Lower Goal</th>
                     <th>Low Climb %</th>
                     <th>Mid Climb %</th>
                     <th>High Climb %</th>
                     <th>Traversal Climb %</th>
                     <th>Total DNP</th>
                     <th>Get Average Score</th>
-                    <th>Avg Teleop Upper Goal</th>
-                    <th>Avg Teleop Lower Goal</th>
-                    <th>Avg Auto Upper Goal</th>
-                    <th>Avg Auto Lower Goal</th>
                     <th>Max Teleop Upper Goal</th>
                     <th>Max Teleop Lower Goal</th>
                     <th>Max Auto Upper Goal</th>
@@ -72,6 +72,10 @@ include("header.php") ?>
                     // $picklist = (getPickList($teamNumber, $min, $max) - getAvgDriveRank($teamNumber, $min, $max));
                     $scoutPick = getElo($teamNumber);
                     $UpperShotPercentage = getAvgUpperShotPercentage($teamNumber, $min, $max);
+                    $avgTeleopUpper = getAvgUpperGoalT($teamNumber, $min, $max);
+                    $avgTeleopLower = getAvgLowerGoalT($teamNumber, $min, $max);
+                    $avgAutoUpper = getAvgUpperGoal($teamNumber, $min, $max);
+                    $avgAutoLower = getAvgLowerGoal($teamNumber, $min, $max);
                     $SingleClimbPer = getSingleClimbPercent($teamNumber, $min, $max);
                     $MidClimbPer = getDoubleClimbPercent($teamNumber, $min, $max);
                     $HighClimbPer = getTripleClimbPercent($teamNumber, $min, $max);
@@ -79,10 +83,6 @@ include("header.php") ?>
                     // $allianceRank = getAvgDriveRank($teamNumber, $min, $max);
                     $DNP = getTotalDNP($teamNumber, $min, $max);
                     $ScoreCont = getAvgScore($teamNumber, $min, $max);
-                    $avgTeleopUpper = getAvgUpperGoalT($teamNumber, $min, $max);
-                    $avgTeleopLower = getAvgLowerGoalT($teamNumber, $min, $max);
-                    $avgAutoUpper = getAvgUpperGoal($teamNumber, $min, $max);
-                    $avgAutoLower = getAvgLowerGoal($teamNumber, $min, $max);
                     $maxTeleopUpper = getMaxUpperGoalT($teamNumber, $min, $max);
                     $maxTeleopLower = getMaxLowerGoalT($teamNumber, $min, $max);
                     $maxAutoUpper = getMaxUpperGoal($teamNumber, $min, $max);
@@ -93,16 +93,16 @@ include("header.php") ?>
                     <th>" . round($picklist) . "</th>
                     <th>" . ($scoutPick) . "</th>
                     <th>" . $UpperShotPercentage . "</th>
+                    <th>" . round($avgTeleopUpper, 3) . "</th>
+                    <th>" . round($avgTeleopLower, 3) . "</th>
+                    <th>" . round($avgAutoUpper, 3) . "</th>
+                    <th>" . round($avgAutoLower, 3) . "</th>
                     <th>" . round($SingleClimbPer, 3) . "</th>
                     <th>" . round($MidClimbPer, 3) . "</th>
                     <th>" . round($HighClimbPer, 3) . "</th>
                     <th>" . round($TravClimbPer, 3) . "</th>
                     <th>" . round($DNP, 3) . "</th>
                     <th>" . round($Score, 3) . "</th>
-                    <th>" . round($avgTeleopUpper, 3) . "</th>
-                    <th>" . round($avgTeleopLower, 3) . "</th>
-                    <th>" . round($avgAutoUpper, 3) . "</th>
-                    <th>" . round($avgAutoLower, 3) . "</th>
                     <th>" . $maxTeleopUpper . "</th>
                     <th>" . $maxTeleopLower . "</th>
                     <th>" . $maxAutoUpper . "</th>
